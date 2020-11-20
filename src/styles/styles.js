a288521b50a282ac94f64c308d9e85a43c976c3b
Samuel Outeda <samuelouteda@gmail.com>
2020-11-20 16:20:18 -0400

    Creation of style rules
    Creation of style rules for sizes and colors

---
+++ src/styles/styles.js
@@ -0,0 +1,25 @@
+export const theme = {
+  colors: {
+    gray1: "rgb(45, 45, 45)",
+    gray2: "rgb(70, 70, 70)",
+    blue1: "rgb(0, 136, 235)",
+    tBlue: "rgba(0, 136, 235, 0.75)",
+  },
+  sizes: {
+    borderThickness: {
+      thick1: 1,
+      thick2: 2,
+      thick3: 4,
+      thick4: 5,
+      thick5: 10,
+      thick6: 15,
+      thick7: 20,
+    },
+    sidebarDimensions: {
+      width: "16%",
+      minWidth: 200,
+      maxWidth: 240,
+    }
+  },
+
+};
