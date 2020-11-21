cf5ff813c67a4e370a248b252994e1eeccf57ab5
Samuel Outeda <samuelouteda@gmail.com>
2020-11-21 15:29:53 -0400

    Adding and editing style rules
    Adding thickness rules and editing color rules

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,28 +1,40 @@
 export const styles = {
   colors: {
-    gray1: "rgb(45, 45, 45)",
-    gray2: "rgb(70, 70, 70)",
-    blue1: "rgb(0, 136, 235)",
-    tBlue: "rgba(0, 136, 235, 0.75)",
+    darkGray: "rgb(45, 45, 45)",
+    mediumGray: "rgb(70, 70, 70)",
+    lightBlue: "rgb(0, 136, 235)",
+    transparentLightBlue: "rgba(0, 136, 235, 0.75)",
   },
   sizes: {
     thickness: {
       thick1: 1,
       thick2: 2,
       thick3: 4,
-      thick4: 5,
-      thick5: 10,
-      thick6: 15,
-      thick7: 20,
-      thick8: 25,
-      thick9: 30,
-      thick10: 35,
-      thick11: 40,
-      thick12: 45,
-      thick13: 50
+      thick4: 6,
+      thick5: 8,
+      thick6: 10,
+      thick7: 12,
+      thick8: 14,
+      thick9: 16,
+      thick10: 18,
+      thick11: 20,
+      thick12: 22,
+      thick13: 24,
+      thick14: 26,
+      thick15: 28,
+      thick16: 30,
+      thick17: 32,
+      thick18: 34,
+      thick19: 36,
+      thick20: 38,
+      thick21: 40,
+      thick22: 42
     },
     specialThick: { //This rule is for really specific numbers. This way, that values do not push the normal ones.
-      thick1: 42
+      spacingThick1: 5,
+      spacingThick2: 15,
+      spacingThick3: 25,
+      spacingThick4: 35
       },
     sidebarDimensions: {
       width: "16%",
