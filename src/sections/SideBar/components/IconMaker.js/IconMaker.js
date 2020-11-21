0de34223ecbd68297ea8ddd2b6650dff94f0c4a1
Samuel Outeda <samuelouteda@gmail.com>
2020-11-20 20:34:46 -0400

    Creation of IconMaker
    Creation of a fourth component of sidebar, the IconMaker, which contains logic to return diferent icons based on received props.

---
+++ src/sections/SideBar/components/IconMaker.js/IconMaker.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { HomeOutlinedIcon, PersonOutlineOutlinedIcon, MailOutlineOutlinedIcon, NotesOutlinedIcon } from "@material-ui/icons";
+import { FacebookIcon, InstagramIcon, LinkedInIcon, GitHubIcon } from '@material-ui/icons';
+
+const IconMaker = (props) => {
+    const selectIcon = (index) => {
+      return index === 0 ? <HomeOutlinedIcon className={props.icon} />
+          :  index === 1 ? <PersonOutlineOutlinedIcon className={props.icon} />
+          :  index === 2 ? <NotesOutlinedIcon className={props.icon} />
+          :  index === 3 ? <MailOutlineOutlinedIcon className={props.icon} />
+          :  index === 4 ? <FacebookIcon className={props.icon} onMouseOver={props.mouseOver} onMouseOut={props.mouseOut} />
+          :  index === 5 ? <InstagramIcon className={props.icon} onMouseOver={props.mouseOver} onMouseOut={props.mouseOut} />
+          :  index === 6 ? <LinkedInIcon className={props.icon} onMouseOver={props.mouseOver} onMouseOut={props.mouseOut} />
+          : <GitHubIcon className={props.icon} onMouseOver={props.mouseOver} onMouseOut={props.mouseOut} />;
+    };
+    return (
+            <div>
+              {selectIcon(props.index)}
+            </div>
+    );
+  };
+  
+  export default IconMaker;
+  
