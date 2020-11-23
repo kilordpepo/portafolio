846fcb7914a8d3d11f6556d258655cbcbb483c93
Samuel Outeda <samuelouteda@gmail.com>
2020-11-23 15:45:08 -0400

    Correcting import
    Correcting the relative path of an import

--- src/sections/SideBar/SideBar.jsx
+++ src/sections/SideBar/SideBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Footer, SelectedListItem, ProfilePic } from "./components/index";
-import { styles } from "../../../../styles/styles";
+import { styles } from "../../styles/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
