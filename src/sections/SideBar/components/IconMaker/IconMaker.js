df811784eb89ec6a0ada72a21ccdedd4a4c98f62
Samuel Outeda <samuelouteda@gmail.com>
2020-11-23 15:44:33 -0400

    Refactoring imports
    Refactoring named imports to be default individual imports, because the @material-ui/icons dependency does not allow named imports for that components

--- src/sections/SideBar/components/IconMaker/IconMaker.js
+++ src/sections/SideBar/components/IconMaker/IconMaker.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { HomeOutlinedIcon, PersonOutlineOutlinedIcon, MailOutlineOutlinedIcon, NotesOutlinedIcon, FacebookIcon, InstagramIcon, LinkedInIcon, GitHubIcon } from "@material-ui/icons";
+import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+import MailOutlineOutlinedIcon from "@material-ui/icons/MailOutlineOutlined";
+import NotesOutlinedIcon from "@material-ui/icons/NotesOutlined";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 const IconMaker = (props) => {
     const selectIcon = (index) => {
